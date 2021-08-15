c7005b6dbd8e7e7e9a2cc7b8942d856b7b122df6
Kisoty <kisoty@gmail.com>
2021-08-15 23:04:09 +0300

    DTO constructor property initialization

--- src/Chat/MessageDTO/ChangeNameDTO.php
+++ src/Chat/MessageDTO/ChangeNameDTO.php
@@ -8,19 +8,16 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class ChangeNameDTO
 {
-    /**
-     * @Assert\NotBlank()
-     * @Assert\Length(
-     *     min=2,
-     *     max=10
-     *     minMessage = "Your first name must be at least {{ limit }} characters long",
-     *     maxMessage = "Your first name cannot be longer than {{ limit }} characters"
-     * )
-     */
-    public string $newName;
-
-    public function __construct(string $newName)
-    {
-        $this->newName = $newName;
-    }
+    public function __construct(
+        /**
+         * @Assert\NotBlank()
+         * @Assert\Length(
+         *     min=2,
+         *     max=10,
+         *     minMessage = "Your first name must be at least {{ limit }} characters long",
+         *     maxMessage = "Your first name cannot be longer than {{ limit }} characters"
+         * )
+         */
+        public string $newName
+    ) {}
 }
--- src/Chat/MessageDTO/CommonMessageDTO.php
+++ src/Chat/MessageDTO/CommonMessageDTO.php
@@ -9,13 +9,10 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CommonMessageDTO
 {
-    /**
-     * @Assert\NotBlank()
-     */
-    public string $message;
-
-    public function __construct(string $message)
-    {
-        $this->message = $message;
-    }
+    public function __construct(
+        /**
+         * @Assert\NotBlank()
+         */
+        public string $message
+    ) {}
 }

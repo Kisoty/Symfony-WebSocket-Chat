ae5788a62f13385463b4833c0fdd61678197b129
Kisoty <kisoty@gmail.com>
2021-08-23 22:14:30 +0300

    ChangeName refactor

--- src/Chat/MessageHandlers/ChangeNameHandler.php
+++ src/Chat/MessageHandlers/ChangeNameHandler.php
@@ -10,21 +10,10 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ChangeNameHandler
 {
-    public function __construct(private ValidatorInterface $validator) {}
+    public function __construct() {}
 
     public function handle(ChatUser $sender, ChangeNameDTO $dto)
     {
-        $errors = $this->validator->validate($dto);
-
-        if (count($errors) > 0) {
-            $errorMsg = '';
-            foreach ($errors as $error) {
-                $errorMsg .= $error->getMessage() . PHP_EOL;
-            }
-
-            $sender->receiveMessage($errorMsg);
-        } else {
-            $sender->changeName($dto->newName);
-        }
+        $sender->changeName($dto->newName);
     }
 }
